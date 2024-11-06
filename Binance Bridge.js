2470966fc625972ba5ae5025982119cfcbfc0d00
pechenka232 <strekalovskijdemid@gmail.com>
2024-11-06 16:33:39 +0800

    Update Binance Bridge.js

--- Binance Bridge.js
+++ Binance Bridge.js
@@ -3,7 +3,7 @@ const Web3 = require('web3');
 const bscProvider = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545');
 const polygonProvider = new Web3('https://rpc-mumbai.maticvigil.com');
 
-const tokenAddress = 'YOUR_TOKEN_ADDRESS';
+const tokenAddress = '';
 const bscTokenContract = new bscProvider.eth.Contract(ABI, tokenAddress);
 const polygonTokenContract = new polygonProvider.eth.Contract(ABI, tokenAddress);
 
